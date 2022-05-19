9341e2df2a6df1d62697a1d2cd39054730271518
Jose Valenzuela <valenz74@gmail.com>
2022-05-19 18:06:40 -0500

    Aprendiendo manejo de Datos con Casting en Java

---
+++ src/DatosConCasting.java
@@ -0,0 +1,12 @@
+
+public class DatosConCasting {
+    public static void main(String[] args) {
+        byte a = 15;
+        byte b = 2;
+        //Casting
+        byte c = (byte) (a + b);
+
+        System.out.println(c);
+
+    }
+}
